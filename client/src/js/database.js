76585151a7a15ddcb557bfafe967d3293a5d1e10
FormulaXYZ <thomas.assefa6@gmail.com>
2022-10-31 09:45:38 -0700

    added result.value in database.js in scc/js folder

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,10 +20,10 @@ export const putDb = async (content) => {
   const contactDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = contactDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
   const request = store.add({ content: content, });
@@ -42,10 +42,10 @@ export const getDb = async () => {
   const contactDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readonly');
+  const tx = contactDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
@@ -53,7 +53,7 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 
